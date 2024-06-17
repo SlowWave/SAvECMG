596d34ecd1db3d4ec427f6f4f751ab3f319034a8
SlowWave <chrs.borgia@gmail.com>
2024-06-17 23:56:41 +0200

    tested CMGA methods

--- savecmg/modules/cmga.py
+++ savecmg/modules/cmga.py
@@ -115,8 +115,74 @@ class ControlMomentGyroAssembly:
 
 if __name__ == "__main__":
 
+    import plotly.graph_objects as go
+
     cmga = ControlMomentGyroAssembly()
+    
+    # print(cmga.get_jacobian(np.array([0, 0, 0, 0])))
+    # print(cmga.get_angular_momentum(np.array([0, 0, 0, 0]), [10000, 1, 1, 1]))
 
-    print(cmga.get_jacobian(np.array([0, 0, 0, 0])))
+    beta = [0, 90, 30, 60]
+    cmga.beta = np.deg2rad(beta)
+    cmga.cmg_array = [False, True, True, True]
+    cmgs_momenta = [1, 1, 1, 1]
 
-    print(cmga.get_angular_momentum(np.array([0, 0, 0, 0]), [10000, 1, 1, 1]))
+    n_points = 100
+    det_limit = 1e-3
+    fig_name = str(beta[1]) + "_" + str(beta[2]) + "_" + str(beta[3]) + "_" + str(n_points) + "pnts.html"
+
+    singular_theta = {
+        "theta_1": [],
+        "theta_2": [],
+        "theta_3": [],
+    }
+    singular_momentum = {
+        "momentum_x": [],
+        "momentum_y": [],
+        "momentum_z": [],
+    }
+
+    for theta_1 in np.linspace(-np.pi, np.pi, n_points):
+        print(np.rad2deg(theta_1))
+        for theta_2 in np.linspace(-np.pi, np.pi, n_points):
+            for theta_3 in np.linspace(-np.pi, np.pi, n_points):
+                jacobian = cmga.get_jacobian(np.array([0., theta_1, theta_2, theta_3]))
+                det = np.linalg.det(jacobian)
+                if np.abs(det) < det_limit:
+                    # print(f"singular: {np.rad2deg(theta_1)}, {np.rad2deg(theta_2)}, {np.rad2deg(theta_3)}")
+                    momentum = cmga.get_angular_momentum(np.array([0., theta_1, theta_2, theta_3]), cmgs_momenta)
+                    singular_momentum["momentum_x"].append(momentum[0])
+                    singular_momentum["momentum_y"].append(momentum[1])
+                    singular_momentum["momentum_z"].append(momentum[2])
+
+                    singular_theta["theta_1"].append(theta_1)
+                    singular_theta["theta_2"].append(theta_2)
+                    singular_theta["theta_3"].append(theta_3)
+
+    fig1 = go.Figure(
+        data=[
+            go.Scatter3d(
+                x=singular_theta["theta_1"],
+                y=singular_theta["theta_2"],
+                z=singular_theta["theta_3"],
+                mode="markers",
+                marker=dict(size=3),
+            )
+        ]
+    )
+    # fig1.show()
+    fig1.write_html("angles_" + fig_name)
+
+    fig2 = go.Figure(
+        data=[
+            go.Scatter3d(
+                x=singular_momentum["momentum_x"],
+                y=singular_momentum["momentum_y"],
+                z=singular_momentum["momentum_z"],
+                mode="markers",
+                marker=dict(size=3),
+            )
+        ]
+    )
+    # fig2.show()
+    fig2.write_html("momentum_" + fig_name)
