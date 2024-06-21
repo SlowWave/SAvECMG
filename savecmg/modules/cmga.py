9e4a8d5fddb706eedd8e19bdfc2fd5a6717a2b62
SlowWave <chrs.borgia@gmail.com>
2024-06-21 11:38:11 +0200

    updated class methods

--- savecmg/modules/cmga.py
+++ savecmg/modules/cmga.py
@@ -4,8 +4,8 @@ import numpy as np
 class ControlMomentGyroAssembly:
     def __init__(self):
 
-        self.beta = np.array([0.0, 0.0, 0.0, 0.0])
-        self.cmg_array = [True, True, True, True]
+        self.cmgs_beta = np.array([0.0, 0.0, 0.0, 0.0])
+        self.cmgs_array = [True, True, True, True]
         self.jacobian = None
         self.angular_momentum = None
         self.torque = None
@@ -24,51 +24,49 @@ class ControlMomentGyroAssembly:
     
     def get_states(self):
         
-        # TODO: return states (jacobian, angular_momentum, torque)
-        
-        pass
+        return self.jacobian, self.angular_momentum, self.torque
     
-    def get_jacobian(self, theta):
+    def get_jacobian(self, cmgs_theta):
 
         jacobian_elements = []
 
-        if self.cmg_array[0]:
+        if self.cmgs_array[0]:
             jacobian_elements.append(
                 np.array(
                     [
-                        -np.cos(self.beta[0]) * np.cos(theta[0]),
-                        -np.sin(theta[0]),
-                        np.sin(self.beta[0]) * np.cos(theta[0]),
+                        -np.cos(self.cmgs_beta[0]) * np.cos(cmgs_theta[0]),
+                        -np.sin(cmgs_theta[0]),
+                        np.sin(self.cmgs_beta[0]) * np.cos(cmgs_theta[0]),
                     ]
                 )
             )
-        if self.cmg_array[1]:
+        if self.cmgs_array[1]:
             jacobian_elements.append(
                 np.array(
                     [
-                        np.sin(theta[1]),
-                        -np.cos(self.beta[1]) * np.cos(theta[1]),
-                        np.sin(self.beta[1]) * np.cos(theta[1]),
+                        np.sin(cmgs_theta[1]),
+                        -np.cos(self.cmgs_beta[1]) * np.cos(cmgs_theta[1]),
+                        np.sin(self.cmgs_beta[1]) * np.cos(cmgs_theta[1]),
                     ]
                 )
             )
-        if self.cmg_array[2]:
+        if self.cmgs_array[2]:
             jacobian_elements.append(
                 np.array(
                     [
-                        np.cos(theta[2]) * np.cos(theta[2]),
-                        np.sin(theta[2]),
-                        np.sin(self.beta[2]) * np.cos(theta[2]),
+                        np.cos(cmgs_theta[2]) * np.cos(cmgs_theta[2]),
+                        np.sin(cmgs_theta[2]),
+                        np.sin(self.cmgs_beta[2]) * np.cos(cmgs_theta[2]),
                     ]
                 )
             )
-        if self.cmg_array[3]:
+        if self.cmgs_array[3]:
             jacobian_elements.append(
                 np.array(
                     [
-                        -np.sin(theta[3]),
-                        np.cos(self.beta[3]) * np.cos(theta[3]),
-                        np.sin(self.beta[3]) * np.cos(theta[3]),
+                        -np.sin(cmgs_theta[3]),
+                        np.cos(self.cmgs_beta[3]) * np.cos(cmgs_theta[3]),
+                        np.sin(self.cmgs_beta[3]) * np.cos(cmgs_theta[3]),
                     ]
                 )
             )
@@ -77,51 +75,51 @@ class ControlMomentGyroAssembly:
 
         return jacobian
 
-    def get_angular_momentum(self, theta, cmgs_momenta):
+    def get_angular_momentum(self, cmgs_theta, cmgs_momenta):
 
         cmgs_momenta = np.delete(
-            cmgs_momenta, np.where(np.array(self.cmg_array) == False)[0]
+            cmgs_momenta, np.where(np.array(self.cmgs_array) == False)[0]  # noqa: E712
         )
 
         rotation_matrix = []
 
-        if self.cmg_array[0]:
+        if self.cmgs_array[0]:
             rotation_matrix.append(
                 np.array(
                     [
-                        -np.cos(self.beta[0]) * np.sin(theta[0]),
-                        np.cos(theta[0]),
-                        np.sin(self.beta[0]) * np.sin(theta[0]),
+                        -np.cos(self.cmgs_beta[0]) * np.sin(cmgs_theta[0]),
+                        np.cos(cmgs_theta[0]),
+                        np.sin(self.cmgs_beta[0]) * np.sin(cmgs_theta[0]),
                     ]
                 )
             )
-        if self.cmg_array[1]:
+        if self.cmgs_array[1]:
             rotation_matrix.append(
                 np.array(
                     [
-                        -np.cos(theta[1]),
-                        -np.cos(self.beta[1]) * np.sin(theta[1]),
-                        np.sin(self.beta[1]) * np.sin(theta[1]),
+                        -np.cos(cmgs_theta[1]),
+                        -np.cos(self.cmgs_beta[1]) * np.sin(cmgs_theta[1]),
+                        np.sin(self.cmgs_beta[1]) * np.sin(cmgs_theta[1]),
                     ]
                 )
             )
-        if self.cmg_array[2]:
+        if self.cmgs_array[2]:
             rotation_matrix.append(
                 np.array(
                     [
-                        np.cos(self.beta[2]) * np.sin(theta[2]),
-                        -np.cos(theta[2]),
-                        np.sin(self.beta[2]) * np.sin(theta[2]),
+                        np.cos(self.cmgs_beta[2]) * np.sin(cmgs_theta[2]),
+                        -np.cos(cmgs_theta[2]),
+                        np.sin(self.cmgs_beta[2]) * np.sin(cmgs_theta[2]),
                     ]
                 )
             )
-        if self.cmg_array[3]:
+        if self.cmgs_array[3]:
             rotation_matrix.append(
                 np.array(
                     [
-                        np.cos(theta[3]),
-                        np.cos(self.beta[3]) * np.sin(theta[3]),
-                        np.sin(self.beta[3]) * np.sin(theta[3]),
+                        np.cos(cmgs_theta[3]),
+                        np.cos(self.cmgs_beta[3]) * np.sin(cmgs_theta[3]),
+                        np.sin(self.cmgs_beta[3]) * np.sin(cmgs_theta[3]),
                     ]
                 )
             )
@@ -130,6 +128,17 @@ class ControlMomentGyroAssembly:
 
         return angular_momentum
 
-    def get_torque(self, jacobian, cmgs_velocities):
-        pass
+    def get_torque(self, jacobian, cmgs_momenta, cmgs_velocities):
+        
+        cmgs_momenta = np.delete(
+            cmgs_momenta, np.where(np.array(self.cmgs_array) == False)[0]  # noqa: E712
+        )
+        
+        cmgs_velocities = np.delete(
+            cmgs_velocities, np.where(np.array(self.cmgs_array) == False)[0]  # noqa: E712
+        )
+        
+        torque = np.dot(np.dot(jacobian, np.diag(cmgs_momenta)), cmgs_velocities)
+        
+        return torque
 
