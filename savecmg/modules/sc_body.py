71fe27db2e8450f22df6bdffbebfd82bbc8c4d06
SlowWave <chrs.borgia@gmail.com>
2024-06-21 10:46:29 +0200

    updated methods prototypes

--- savecmg/modules/sc_body.py
+++ savecmg/modules/sc_body.py
@@ -3,3 +3,15 @@
 class SpacecraftBody:
     def __init__(self):
         pass
+    
+    def propagate_states(self, cmga_torque):
+        
+        pass
+    
+    def get_states(self):
+        
+        pass
+
+    def _ode(self):
+        
+        pass
